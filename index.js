8b4d5835ddda60890934155ba83e3a87d43894c4
Mac Pro <macpro@Macs-MacBook-Pro.local>
2024-11-05 21:15:36 +0100

    add new front end improvements, add time functionality , add top nav bar

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get("/",async (req,res)=>{
     try {
        const response = await axios("https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=20");
 const result = response.data;
-res.render("index.ejs",{content:result}); 
+res.render("index.ejs",{content:result,fulltime:fullTime}); 
 console.log("correct");
     } catch (error) {
         console.log(error.message.data);
@@ -29,6 +29,19 @@ console.log("correct");
 app.listen(port,()=>{
 console.log("Server listening the port :"+port);
 });
+
+//get the current full date using a function
+function fullTime(){
+    const year = new Date().getFullYear();
+    const month = new Date().getMonth();
+    const day = new Date().getDate();
+    const hour = new Date().getHours(); 
+    const minutes = new Date().getMinutes();
+    const seconds = new Date().getSeconds();
+
+    return day+"/"+month+"/"+year+" | "+hour+":"+minutes+":"+seconds;
+}
+
 
 
 
